07e4d796723cbb631667a58c0e85e34839104d7b
Phillip Short <phillipshort@Phillips-MBP.lan>
2022-07-18 20:58:12 +0100

    removed schedule and time modules and made use of crontabs to schedule running of script, also included hidden file check

--- wallpaper_changer.py
+++ wallpaper_changer.py
@@ -1,13 +1,13 @@
+#this code originally imported the schedule and time modules to run code a set intervals however I got mixed results with this over longer time durations so I abandoned this approach, instead I made use of crontabs on my OS to run this script once a day.
+
 #import relevant modules
-import time
-import schedule
 import os
 import pathlib
 import random
 import subprocess
 
-#variable to hold folder name, change this to the name of your local folder containing wallpaper backgrounds
-WALLPAPPER_DIR = 'wallpapers'
+#variable to hold paths to wallpaper backgrounds
+WALLPAPPER_DIR = '/Users/phillipshort/Desktop/python/wallpaper_changer/wallpapers'
 
 #variable that stores apple script code to select wallpaper file
 SET_WALLPAPER_OSASCRIPT = """/usr/bin/osascript<<END
@@ -20,25 +20,17 @@ END"""
 def change_wallpaper():
     #select a wallpaper at random from the wallpaper directory
     rand_wallpaper = random.choice(os.listdir(WALLPAPPER_DIR))
-    #check that file name is valid
-    if (rand_wallpaper != '.DS_Store'):
-        #if so print change to terminal & create absolute path to file for apple script code
-        print(f"changing desktop wallpaper to: {rand_wallpaper}")
+    #variable to hold weather file starts with a '.' or not, used to detect the randomly selected file is a hidden file or not
+    hidden_file = rand_wallpaper.startswith('.')
+    #check and proceed if file isnt hidden
+    if hidden_file == False:
+        #if so print create absolute path to file for apple script code
         full_image_path = os.path.join(
             pathlib.Path().absolute(), WALLPAPPER_DIR, rand_wallpaper
         )
         #pass full image path to subprocess so apple script code is formated and executed
         subprocess.Popen(SET_WALLPAPER_OSASCRIPT.format(full_image_path), shell=True)
     
-#function go is called every x unit of time using the schedule module
-def go():
-    #call the change_wallpaper function ever 7 seconds
-    schedule.every(7).seconds.do(change_wallpaper)
-    #while script is running check if an event is pending, then sleep for 1 second
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
-#once file is run call go function
+#run change_wallpaper function
 if __name__ == '__main__':
-    go()
+    change_wallpaper()
